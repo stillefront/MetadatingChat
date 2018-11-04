2bfc9555331416d499ed80239bceb7069bc26a14
alexejb <stillefront@users.noreply.github.com>
2018-11-04 23:48:48 +0100

    some tryouts with async functionalities

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/login');
-var registerRouter = require('./routes/register');
-var insertRouter = require('./routes/insert');
-var successRouter = require('./routes/success');
-var adminRouter = require('./routes/admin');
-var botRouter = require('./routes/bots');
-var chatRouter = require('./routes/chat');
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
+const insertRouter = require('./routes/insert');
+const successRouter = require('./routes/success');
+const adminRouter = require('./routes/admin');
+const botRouter = require('./routes/bots');
+const chatRouter = require('./routes/chat');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/success.js
+++ routes/success.js
@@ -6,7 +6,9 @@ mongoose.connect('mongodb://localhost:27017/botDB')
 var db = mongoose.connection;
 var Login = require('../models/login');
 
-var test;
+
+var nameTest;
+var pwTest;
 
 
 
@@ -24,11 +26,31 @@ Login.findOne({}, {}, { sort: { '_id' : -1 }}, function(err, recent) {
 
   test = recent.username;
   console.log("Testfall:", recent.username);
+  console.log("Testfall:", recent.password);
 
 });
 
-  console.log("Testfall 2: ", test);
-  res.render('success');
+
+//JavaScript Promises
+
+const p = new Promise(function(resolve, reject){
+  resolve(Login.findOne({}, {}, {sort : { '_id' : -1}}, function(err, recent){
+    nameTest = recent.username;
+    pwTest = recent.password;
+  }));
+
+  reject(new Error('hat nicht geklappt'));
+});
+
+p.then(function(result){
+  //var nameTest = result.username;
+  console.log('Testfall 2: ', nameTest);
+  console.log('Testfall 2: ', pwTest);
+});
+
+
+res.render('success', {item: nameTest, item2: pwTest});
+
 
 });
 
